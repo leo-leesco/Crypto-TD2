440b09a43407516e2a16e6d73b5a9de795953bb8
Léo LEESCO <leo.leesco@gmail.com>
2025-01-23 22:05:35 +0100

    gen part

--- src/bin/poly1305-gen.rs
+++ src/bin/poly1305-gen.rs
@@ -1,11 +1,24 @@
+use std::fs;
+
+use TD2::{bernstein::poly1305, convert::extract_rs};
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let mut args = std::env::args();
     assert_eq!(args.len(), 3);
-    let rs: String;
-    let path_to_file: String;
-    unsafe {
-        rs = args.get_unchecked(1).to_string();
-        path_to_file = args.get_unchecked(2).to_string();
-    }
-    println!("rs={rs} ; path_to_file={path_to_file}");
+    let (r, s) = extract_rs(
+        &args
+            .nth(1)
+            .expect("First argument should be the hexadecimal key"),
+    );
+    let m = fs::read(
+        args.nth(2)
+            .expect("Second argument should be the path to the file to be authenticated"),
+    )
+    .expect("Could not read file");
+
+    eprintln!("R = {r:032x}");
+    eprintln!("S = {s:032x}");
+    eprintln!("Tried to open = {}", args.nth(2).unwrap());
+
+    println!("{:032X}", poly1305(&m, r, s));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 #![allow(non_snake_case)]
+
+pub const KEY_SIZE: usize = 16;
+
+pub mod bernstein;
 pub mod convert;
 pub mod polynom;

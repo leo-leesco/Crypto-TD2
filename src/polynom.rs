d3e0c8a3c9f29836d64f17e1763cc5585a24f690
Léo LEESCO <leo.leesco@gmail.com>
2025-01-23 10:00:36 +0100

    refactor testing setup

--- src/polynom.rs
+++ src/polynom.rs
@@ -1,6 +1,16 @@
+// credit to https://docs.rs/fast_polynomial/latest/fast_polynomial/#motivation
 /// coefficients should be ordered the highest degree to lowest
 pub fn polynom_evaluation(x: u32, coefficients: &[u32], p: u32) -> u32 {
     coefficients
         .iter()
         .fold(0, |evaluation, coef| (x * evaluation + coef) % p)
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn horner() {
+        assert_eq!(polynom_evaluation(5, &[1, 2, 3, 4], 1000), 194); // P(5) % 1000 where P = X^3 + 2X^2 + 3X + 4
+    }
 }
--- tests/test.rs
+++
@@ -1,7 +0,0 @@
-use TD2::conversion::*;
-use TD2::polynom::polynom_evaluation;
-
-#[test]
-fn horner() {
-    assert_eq!(polynom_evaluation(5, &[1, 2, 3, 4], 1000), 194); // P(5) % 1000 where P = X^3 + 2X^2 + 3X + 4
-}

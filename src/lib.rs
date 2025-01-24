91fdb7ebec29ed8e88d9ed2a1b257ac93fbbd346
Léo LEESCO <leo.leesco@gmail.com>
2025-01-24 12:01:22 +0100

    remove unnecessary exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
 
-pub const KEY_SIZE: usize = 16;
-pub const LIMBS: usize = 5;
-pub const LIMB_SIZE: usize = 26;
+const KEY_SIZE: usize = 16;
+const LIMBS: usize = 5;
+const LIMB_SIZE: usize = 26;
 
 pub mod bernstein;
 pub mod convert;
-pub mod polynom;
+mod polynom;
